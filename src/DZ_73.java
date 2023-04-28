d45effc12ac591ec09da1df09d862fdc3ca5010f
rak15 <rak1529916@mail.ru>
2023-04-28 21:48:38 +0300

    1

--- src/DZ_73.java
+++ src/DZ_73.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class DZ_73 {
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Objects;
+
+public class DZ_73 {
+    public static void main(String[] args) {
+        String f = "C:\\Users\\rak15\\IdeaProjects\\DZ_73\\Папка";
+      try {
+          File file = new File(f);
+          for (File file1 : Objects.requireNonNull(file.listFiles())) {
+              BasicFileAttributes basicFileAttributes = Files.readAttributes(file1.toPath(), BasicFileAttributes.class);
+              System.out.printf("Name = %s; Length = %d; Is directory = %b ", file1.getName(), basicFileAttributes.size(), basicFileAttributes.isDirectory());
+              System.out.println();
+          }
+      } catch (Exception e) {
+          throw new RuntimeException(e);
+      }
+    }
 }
