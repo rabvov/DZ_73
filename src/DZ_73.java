dc30824b05bc15aec7e1be22731b8ee95463db5c
rak15 <rak1529916@mail.ru>
2023-05-01 12:22:34 +0300

    1.3

--- src/DZ_73.java
+++ src/DZ_73.java
@@ -5,14 +5,12 @@ import java.util.Objects;
 public class DZ_73 {
     public static void main(String[] args) {
         String f = "C:\\Users\\rak15\\IdeaProjects\\DZ_73\\Папка";
-      try {
-          File file = new File(f);
-          for (File file1 : Objects.requireNonNull(file.listFiles())) {
-              System.out.printf("Name = %s; Length = %d; Is directory = %b ", file1.getName(), file1.length(), file1.isDirectory());
-              System.out.println();
-          }
-      } catch (NullPointerException nullPointerException) {
-          throw new NullPointerException();
-      }
-    }
-}
+        File file = new File(f);
+        try {
+           Direct.form(file);
+            } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }}
---
+++ src/Direct.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Direct {
+    public static void form (File f){
+        String d;
+       for (File file: Objects.requireNonNull(f.listFiles())){
+           if (file.isDirectory()){
+               System.out.print("Это директория: ");
+               System.out.printf("Name = %s; Length = %d; Is directory = %b ", file.getName(), file.length(), file.isDirectory());
+               System.out.print("/");
+               System.out.println("В ней есть: "+ Arrays.toString(file.list()));
+               d=file.getAbsolutePath();
+               f= new File(d);
+               Direct.form(f);
+           System.out.println();}
+           else {
+               System.out.print("Это файл: ");
+               System.out.printf("Name = %s; Length = %d; Is directory = %b ", file.getName(), file.length(), file.isDirectory());
+               System.out.println("Находиься в "+file.getParentFile());
+               System.out.println();
+           }
+       }
+
+    }
+}
